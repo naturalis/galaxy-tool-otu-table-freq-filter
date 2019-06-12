ef439231b2e11074a1520f2f00cef0ca6bedc19b
MartenHoogeveen <martenhoogeveen@gmail.com>
2019-06-12 13:59:16 +0000

    new galaxy version update

--- freq_filter_otutable.sh
+++ freq_filter_otutable.sh
@@ -5,4 +5,5 @@
 #location for the testserver
 #outlocation=$(mktemp -d /media/GalaxyData/files/XXXXXX)
 #outlocation=$(mktemp -d /home/galaxy/ExtraRef/XXXXXX)
-freq_filter_otutable.py -i $1 -f $2 -ot $3 -ol $4
+SCRIPTDIR=$(dirname "$(readlink -f "$0")")
+python $SCRIPTDIR"/freq_filter_otutable.py" -i $1 -f $2 -ot $3 -ol $4
