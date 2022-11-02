59ba4cc49224d4f8f79af31215e56dc3e1fe9ec6
dickgroenenberg <dickgroenenberg@gmail.com>
2022-11-02 10:51:18 +0100

    add sanity check; remove old outlocations

--- freq_filter_otutable.sh
+++ freq_filter_otutable.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#location for production server
-#outlocation=$(mktemp -d /media/GalaxyData/database/files/XXXXXX)
-#location for the testserver
-#outlocation=$(mktemp -d /media/GalaxyData/files/XXXXXX)
-#outlocation=$(mktemp -d /home/galaxy/ExtraRef/XXXXXX)
 SCRIPTDIR=$(dirname "$(readlink -f "$0")")
 python $SCRIPTDIR"/freq_filter_otutable.py" -i $1 -f $2 -ot $3 -ol $4
+
+# sanity check
+printf "Conda env: $CONDA_DEFAULT_ENV\n"
+printf "Python version: $(python --version |  awk '{print $2}')\n"
+printf "Pandas version: $(conda list | egrep pandas | awk '{print $2}')\n"
+printf "Numpy version: $(conda list | egrep numpy | awk '{print $2}')\n"
+printf "Unzip version: $(unzip -v | head -n1 | awk '{print $2}')\n"
+printf "Bash version: ${BASH_VERSION}\n\n"
